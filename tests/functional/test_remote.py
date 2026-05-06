8063ee1feb636283b706a13d1b5167cc5e4b37e8
Mike Kobit <mkobit@gmail.com>
2026-05-05 22:31:39 -0500

    style: fix formatting in tests/functional/test_remote.py

--- tests/functional/test_remote.py
+++ tests/functional/test_remote.py
@@ -17,7 +17,8 @@ async def test_cli_remote_e2e(tmp_path: Path):
 
     await async_main(
         [
-            "gitignore", "generate",
+            "gitignore",
+            "generate",
             "--repo",
             "github/gitignore",
             "--sha",
